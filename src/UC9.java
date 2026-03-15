f0093037bba9ac2971c03672ca2162cc352c9abd
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-03-15 15:01:13 +0530

    UC9: Palindrome using stack approach

---
+++ src/UC9.java
@@ -0,0 +1,29 @@
+public class UC9 {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+
+    // Recursive function
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don't match
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return check(s, start + 1, end - 1);
+    }
+}
